30147ad870dfae8ed24cd01c3c0d20a8bea868d3
Julia G. de Aledo <75074514+juliagdealedo@users.noreply.github.com>
2025-11-13 20:03:50 +0100

    order

--- scripts_alumnos/Julia/git.R
+++ scripts_alumnos/Julia/git.R
@@ -4,9 +4,12 @@ install.packages("gitcreds")
 library(usethis)
 library(gitcreds)
 
-usethis::git_remotes()
+# Git
 use_git_config(user.name = "", user.email = "")
 
 # HTTPs
 usethis::create_github_token()
 gitcreds::gitcreds_set()
+
+
+usethis::git_remotes()
